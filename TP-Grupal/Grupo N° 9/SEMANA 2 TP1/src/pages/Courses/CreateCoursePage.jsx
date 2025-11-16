c04e4fe0eac72f563ee76b4ad9fdbcc41c04a551
Luis Emilio Nunez Salvador <luisemiliojp@gmail.com>
2025-11-15 23:44:07 -0300

    fix: unifica imports a endpoints/elindex y evita conflicto de casing

---
+++ TP-Grupal/Grupo N° 9/SEMANA 2 TP1/src/pages/Courses/CreateCoursePage.jsx
@@ -0,0 +1,10 @@
+import FormCourse from "../../components/ui/FormCourse";
+
+export default function CreateCoursePage() {
+  return (
+    <>
+      <h2>📝 Crear Nuevo Curso</h2>
+      <FormCourse />
+    </>
+  );
+}
